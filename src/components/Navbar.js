35d3bab02f50b5f7b9dc6fcfc79fd78145eb5a2f
rookiemonkey <kevinroirigorbasina@pm.me>
2020-10-09 17:55:51 +0800

    tweaked styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import logout from '../helpers/logout'
@@ -16,13 +16,13 @@ const NavigationBar = () => {
                     <span id="warbler"><strong>Warbler</strong></span>
                 </Link>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-list" fill="white" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-list" fill="white" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                     </svg>
                 </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
                         {
                             // conditional rendering for logged-in users
--- src/components/TimelineNoUser.js
+++ src/components/TimelineNoUser.js
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import MessageItem from './mini/MessageItem';
+import Loader from './mini/Loader';
+import fetchMessage from "../helpers/setMessages";
 
-const TimelineNoUser = props => {
+const TimelineNoUser = () => {
+    const dispatch = useDispatch();
+    const messages = useSelector(state => state.messageReducer)
+    const [messagesIsLoading, setMessagesIsLoading] = useState(true);
+
+    useEffect(() => {
+        (async function () {
+            await dispatch(await fetchMessage());
+            await setMessagesIsLoading(false);
+        })()
+    }, [])
 
     return (
+        <React.Fragment>
 
-        <header className="home-hero">
-            <h1>What's happening?</h1>
-            <h4>New to Warbler?</h4>
-            <Link to="/signup">
-                <button className="btn btn-primary">Sign up here</button>
-            </Link>
-        </header>
+            <header className="home-hero">
+                <h1>What's happening?</h1>
+                <h4>New to Warbler?</h4>
+                <Link to="/signup">
+                    <button className="btn btn-primary">Sign up here</button>
+                </Link>
+            </header>
 
+            <div className="container mt-3 p-0" id="timelinenouser-container">
+                <div className="row">
+                    <div className="col-sm-7" id="timelinenouser-message-list">
+                        <h3>Latest on Warbler</h3>
+                        <ul className="list-group" id="timelinenouser-messages">
+                            {
+                                !messagesIsLoading
+                                    ? messages.map(m => (
+                                        <MessageItem
+                                            key={m._id}
+                                            date={m.createAt}
+                                            text={m.text}
+                                            username={m.user.username}
+                                            messageID={m._id}
+                                            authorID={m.user._id}
+                                            profileImageUrl={m.user.profilePicture}
+                                        />
+                                    ))
+                                    : <Loader />
+                            }
+                        </ul>
+                    </div>
+
+                    <div className="col-sm-5">
+                        <h3>Local</h3>
+                    </div>
+                </div>
+
+            </div>
+
+        </React.Fragment>
     );
 
 };
